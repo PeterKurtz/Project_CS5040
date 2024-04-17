565b56d8a028d2c45ba0b26a85541ca310ef9a26
Richard Snider <lighthousemaniac@gmail.com>
2024-04-16 23:22:40 -0600

    finishing touches

--- seagull.py
+++ seagull.py
@@ -2,6 +2,7 @@ import sys
 from functools import cache
 import pandas as pd
 import geopandas as gpd
+from pandas.core.arrays.datetimes import datetime
 from shapely.geometry import Point
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
@@ -40,7 +41,7 @@ for category, group in df.groupby('bird_name'):
     geometry = [Point(xy) for xy in zip(group['longitude'], group['latitude'])]
     gdfs[category] = gpd.GeoDataFrame(group, geometry=geometry, crs="EPSG:4326")
 
-fig, ax = plt.subplots(figsize=(16, 9), dpi=120)
+fig, ax = plt.subplots(figsize=(9, 15), dpi=120)
 ax.set_aspect('equal')
 
 ax.set_xlim(-20, 10)
@@ -53,33 +54,38 @@ for category, gdf in gdfs.items():
     artists[category] = ax.scatter([], [], marker='o', label=category, color=COLORS[category])
     artists[f"arrow_{category}"] = ax.arrow(0, 0, 0, 0, color=COLORS[category], width=0.04, head_width=0.2)
 
+artists["text"] = ax.text(-19, 54, "", fontfamily="monospace")
+
 wind_data = WindData("weather.zip")
 
-def update(frame):
+last_time = dateStart
 
-    if frame >= pd.to_datetime('2014-04-30 23:51:29+00:00'):
+def update(frame):
+    global last_time
+
+    if frame < last_time:
         ani.event_source.stop()
         plt.close(fig)
+        sys.exit(0) # I had to make it exit uncleanly because render wouldn't work with just the other code
+
+    last_time = frame
+
+    artists["text"].set_text(frame.strftime("%Y-%m-%d %H:%M:%S"))
 
     for category, gdf in gdfs.items():
-
         print(frame)
 
         points = gdf[gdf['date_time'] <= frame].geometry.values
-
         if len(points) > 0:
-
             last_point = points[-1]
-
             dx, dy = wind_data.get_wind_vector(frame, last_point.x, last_point.y)
 
             artists[category].set_offsets([(last_point.x, last_point.y)])
-
             artists[f"arrow_{category}"].set_data(x=last_point.x, y=last_point.y, dx=dx * ARROW_SCALE_FACTOR, dy=dy * ARROW_SCALE_FACTOR)
 
     return list(artists.values())
 
-ani = FuncAnimation(fig, update, frames=df['date_time'], blit=True, repeat = False)
+ani = FuncAnimation(fig, update, frames=df['date_time'], blit=True, repeat=False)
 
 ax.legend()
 
